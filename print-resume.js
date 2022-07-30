5f3572aa01cf2ab12dc95459f8c897bf9a563241
Will Warner <47200097+cofad@users.noreply.github.com>
2022-07-30 13:28:48 +0000

    add version to output file name

--- print-resume.js
+++ print-resume.js
@@ -1,14 +1,20 @@
-import { launch } from 'puppeteer';
+import { launch } from "puppeteer";
+import fs from "fs";
 
 (async () => {
+  const packageJson = JSON.parse(fs.readFileSync("./package.json").toString());
+
   // Only use this config for trusted code
   const browser = await launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
   await page.goto(`file://${process.cwd()}/src/resume.html`);
-  await page.pdf({path: 'resume.pdf', format: 'letter'});
+  await page.pdf({
+    path: `resume-${packageJson.version}.pdf`,
+    format: "letter",
+  });
 
   await browser.close();
 })();
