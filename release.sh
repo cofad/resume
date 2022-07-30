5fb2011936e417912dad33ae5d9017ebd1c8a8ea
Will Warner <47200097+cofad@users.noreply.github.com>
2022-07-30 18:20:54 +0000

    use dynamic version

--- release.sh
+++ release.sh
@@ -1,1 +1,10 @@
-gh release create 1.6.0 --notes "release 1.6.0" './resume-1.6.0.pdf'
+npm run build
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | xargs) # trims whitespace
+
+gh release create $PACKAGE_VERSION --notes "release $PACKAGE_VERSION" "./resume-${PACKAGE_VERSION}.pdf"
