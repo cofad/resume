3c0aa814e51c0ab88bf15594e5a0b48e87c3f08f
Will Warner <47200097+cofad@users.noreply.github.com>
2022-08-02 02:51:12 +0000

    return pdf from server

--- resume-api/server.js
+++ resume-api/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 
+const GITHUB_URL = "https://api.github.com/repos/cofad/resume/releases/latest";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const GITHUB_URL = "https://api.github.com/repos/cofad/resume/releases/latest";
-
-app.get(["/releases/latest/pdf"], async (req, res) => {
-  const response = await fetch(GITHUB_URL);
-  const json = await response.json();
-  res.send(json.assets[0].browser_download_url);
+app.use(["/latest"], async (req, res) => {
+  const pdf = await fetchPdf();
+  res.contentType("application/pdf");
+  res.send(pdf);
 });
 
 app.listen(port, () =>
   console.log(`Cofad resume api listening on port ${port}!`)
 );
+
+async function fetchPdf() {
+  const pdfDownloadUrl = await fetchGitHubLatestReleaseAssetUrl();
+  return await fetchPdfFromGitHub(pdfDownloadUrl);
+}
+
+async function fetchGitHubLatestReleaseAssetUrl() {
+  const response = await fetch(GITHUB_URL);
+  const json = await response.json();
+  return json.assets[0].browser_download_url;
+}
+
+async function fetchPdfFromGitHub(url) {
+  const pdfResponse = await fetch(url);
+  const pdfArrayBuffer = await pdfResponse.arrayBuffer();
+  return Buffer.from(pdfArrayBuffer);
+}
